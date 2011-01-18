4314ec7970218fd9793057968d28900e61023dac
Felix Richter <Felix.Richter@syntax-fehler.de>
2011-01-18 01:34:31 +0100

    added unique key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 import pika
-import json,argparse
+import json,argparse,hashlib,sys
 from mail_proc import StatsForUser
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -9,13 +9,17 @@ log = logging.getLogger('proc_mail')
 SRC_QUEUE='mail_proc'
 SRC_EXCHANGE='mailsrc'
 PROC_EXCHANGE='mail_proc'
-
-parser = argparse.ArgumentParser(description='generates dummy package on given exchange against AMQP')
+PROTO_VERSION='1'
+parser = argparse.ArgumentParser(description='performes statistical analysis against mails from stream ')
 parser.add_argument('--host',default='141.31.8.11',      help='AMQP host ip address')
 parser.add_argument('--port',type=int,default=5672,      help='AMQP host port')
 parser.add_argument('-u','--username',default='guest',   help='AMQP username') 
 parser.add_argument('-p','--password',default='guest',   help='AMQP password') 
+parser.add_argument('--unique-key',action='store_true',   help='Unique Key') 
 args = parser.parse_args()
+if args.unique_key:
+  print hashlib.sha1(PROTO_VERSION+args.host+str(args.port)).hexdigest()
+  sys.exit(0)
 
 connection = pika.AsyncoreConnection(pika.ConnectionParameters(
           credentials = pika.PlainCredentials(args.username,args.password), 
